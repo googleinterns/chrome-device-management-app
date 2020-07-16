f2e732fbb589c622404b7ca11d2c3efa7c67dccb
Patricio Andres Saldivar Flores <43447994+PatricioSaldivar@users.noreply.github.com>
2020-07-15 21:59:09 -0500

    Added disk volume report, system ram free, and volume object classes (#26)
    * Added Active time ranges, Basic device, Cpu status report, and Cpu temperature info object classes
    
    * Corrected test label
    
    * Added object classes of device files, last known network, recent users, and tpm version info
    
    * Added object classes and unit tests for disk volume report, system ram free, and volume info
    
    * Added apache license to active time ranges, basic device, cpu status report and cpu temperature info classes
    
    * Added apache license to device files, last known network, recent users and tpm version info classes
    
    * Added license to disk volume report, system ram free report and volume info classes
    
    * Added documentation and made private variables to active time ranges, basic device, cpu status report, cpu temperature info object classes, renamed Objects folder to objects and renamed Unit_Tests_Objects folder to unit_tests_objects
    
    * Added documentation and made private variables to device files, last known network, recent users, tpm version info object classes
    
    * Renamed unit test folder
    
    * Added documentation and made private variables to disk volume report, system ram free report, volume info object classes

---
+++ test/unit_tests/objects/disk_volume_report_test.dart
@@ -0,0 +1,43 @@
+/// Copyright 2020 Google LLC
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     https://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+
+import 'dart:convert';
+import 'package:chrome_management_app/objects/disk_volume_report.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('Check Json parse for volume info', () {
+    final dummy = DiskVolumeReport.fromJson(json.decode("""
+      {
+        "volumeInfo": [
+          {
+            "volumeId": "volume id 1",
+            "storageTotal": "storage total 1",
+            "storageFree": "storage free 1"
+          },
+          {
+            "volumeId": "volume id 2",
+            "storageTotal": "storage total 2",
+            "storageFree": "storage free 2"
+          }
+        ]
+      }
+    """));
+    for (int i = 0; i < dummy.volumeInfo.length; i++) {
+      expect(dummy.volumeInfo[i].volumeId, 'volume id ${i + 1}');
+      expect(dummy.volumeInfo[i].storageTotal, 'storage total ${i + 1}');
+      expect(dummy.volumeInfo[i].storageFree, 'storage free ${i + 1}');
+    }
+  });
+}
---
+++ test/unit_tests/objects/system_ram_free_report_test.dart
@@ -0,0 +1,36 @@
+/// Copyright 2020 Google LLC
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     https://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+
+import 'dart:convert';
+import 'package:chrome_management_app/objects/system_ram_free_report.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('Check Json parse for system ram free report', () {
+    final dummy = SystemRamFreeReport.fromJson(json.decode("""
+      {
+        "reportTime": "report time",
+        "systemRamFreeInfo": 
+        [
+          "system ram free info 1",
+          "system ram free info 2"
+        ]
+      }
+    """));
+    expect(dummy.reportTime, 'report time');
+    for (int i = 0; i < dummy.systemRamFreeInfo.length; i++) {
+      expect(dummy.systemRamFreeInfo[i], 'system ram free info ${i + 1}');
+    }
+  });
+}
