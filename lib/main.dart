582395edb67de4c303fb99162df92c1736071cf7
PatricioSaldivar <patricio.saldivarf@gmail.com>
2020-07-17 19:20:49 -0500

    Added root widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,17 @@ import 'package:flutter/material.dart';
 
 /// Intial function runs into Google Sign In view.
 void main() {
-  runApp(LogIn());
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  // Build a root widget and navigate it to LogIn view so it
+  // can navigate further on to other views.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LogIn(),
+    );
+  }
 }
