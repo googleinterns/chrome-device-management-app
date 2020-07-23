6f504523362fb9669022b4887ef3d7e495b8fdd0
Patricio Andres Saldivar Flores <43447994+PatricioSaldivar@users.noreply.github.com>
2020-07-23 15:38:24 -0500

    Added Api calls model (#28)
    * Added Api Calls Model with its unit test
    
    * Renamed Models folder to models in objects and unit_tests folders, and made private variables in api_calls model
    
    * Changed url variables to constants
    
    * Made Api model instantiable, added issue to TODO
    
    * Modified API Calls tests
    
    * Cleaner code

---
+++ lib/models/api_calls.dart
@@ -0,0 +1,102 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:chrome_management_app/objects/account_devices.dart';
+import 'package:chrome_management_app/objects/detailed_device.dart';
+
+/// The model will manage all the API calls to the Directory API.
+///
+/// See more
+/// https://developers.google.com/admin-sdk/directory/v1/guides/manage-chrome-devices.
+
+class ApiCalls {
+  /// Http client that will do the https requests.
+  http.Client _client;
+
+  ApiCalls(this._client);
+
+  /// Url prefix
+  static const PREFIX = 'https://www.googleapis.com/admin/directory/v1';
+
+  ///Url to get the intial page of the list
+  static const DEVICE_LIST_URL =
+      '/customer/my_customer/devices/chromeos?projection=BASIC&maxResults=15';
+
+  ///Url to get the next page of the list of devices with a next page token
+  static const DEVICE_LIST_WITH_TOKEN_URL =
+      '/customer/my_customer/devices/chromeos?projection=BASIC&maxResults=15&pageToken=';
+
+  ///Url to get detailed information of a device
+  static const DETAILED_DEVICE_URL = '/customer/my_customer/devices/chromeos/';
+
+  /// Calls the Directory API to get a list of devices with max results of 15.
+  ///
+  /// * [authToken] is the authentication token for API to accept the requests.
+  /// * [nextPageToken]  is the next page of the devices list that should
+  /// retreive, if it is null the function will request the first page.
+  ///
+  /// This method returns a [AccountDevices] object.
+  getDeviceList(String authToken, String nextPageToken) async {
+    var response;
+    var url = nextPageToken == null
+        ? PREFIX + DEVICE_LIST_URL
+        : PREFIX + DEVICE_LIST_WITH_TOKEN_URL + nextPageToken;
+    authToken = 'Bearer $authToken';
+    try {
+      response = await _client
+          .get(url, headers: {HttpHeaders.authorizationHeader: authToken});
+      // If conection is succesful return a AccountDevices object by parsing the
+      // response body
+      if (response.statusCode == 200) {
+        return AccountDevices.fromJson(json.decode(response.body));
+      } else {
+        // If that call was not successful, throw an error.
+        throw Exception(response.statusCode);
+      }
+    } finally {
+      _client.close();
+    }
+  }
+
+  /// Calls the Directory API to get the detailed information of a device.
+  ///
+  /// * [authToken] is the authentication token for API to accept the requests.
+  /// * [deviceId] is the ID of the device which its detailed information
+  /// will be retrieved.
+  ///
+  /// This method returns a [DetailedDevice] object.
+  dynamic getDetailedDevice(String authToken, String deviceId) async {
+    authToken = 'Bearer $authToken';
+    try {
+      final response = await _client.get(
+          PREFIX + DETAILED_DEVICE_URL + deviceId,
+          headers: {HttpHeaders.authorizationHeader: authToken});
+      // If conection is succesful return a DetailedDevice object by parsing the
+      // response body
+      if (response.statusCode == 200) {
+        return DetailedDevice.fromJson(json.decode(response.body));
+      } else {
+        // If that call was not successful, throw an error.
+        throw Exception(response.statusCode);
+      }
+    } finally {
+      _client.close();
+    }
+  }
+
+  // TODO Remote commands Issue #38
+}
---
+++ test/unit_tests/models/api_calls_test.dart
@@ -0,0 +1,163 @@
+/// Copyright 2020 Google LLC
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     https://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+
+import 'dart:io';
+import 'package:chrome_management_app/objects/account_devices.dart';
+import 'package:chrome_management_app/objects/detailed_device.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:http/http.dart' as http;
+import 'package:chrome_management_app/models/api_calls.dart';
+
+/// Class to mock a http client
+class MockClient extends Mock implements http.Client {}
+
+main() {
+  /// Mock a authentication token
+  final authToken = '123';
+
+  /// Mock a next page token
+  final nextPageToken = '456';
+
+  /// Mock a device ID
+  final deviceId = '789';
+
+  /// Mock json response from [deviceList] and [deviceListWithTokenUrl]
+  final jsonAccountDevices = """
+    {
+    "kind": "kind",
+    "etag": "etag",
+    "chromeosdevices": [
+      {
+        "kind": "kind 1",
+        "etag": "etag 1",
+        "deviceId": "device Id 1",
+        "serialNumber": "serialNumber 1",
+        "status": "status 1",
+        "lastSync": "lastSync 1", 
+        "supportEndDate": "supportEndDate 1",
+        "annotatedUser": "annotated user 1",
+        "annotatedLocation": "annotated location 1",
+        "annotatedAssetId": "anotated asset id 1",
+        "notes": "notes 1"       
+      }
+    ],
+    "nextPageToken": "token"
+    }""";
+
+  /// Mock json response from [detailedDeviceUrl]
+  final jsonDetailedDevice = """
+    {
+    "kind": "kind",
+    "etag": "etag",
+    "deviceId": "device ID",
+    "serialNumber": "serial number",
+    "status": "status",
+    "lastSync": "last sync",
+    "supportEndDate": "support end date",
+    "annotatedUser": "user",
+    "annotatedLocation": "location",
+    "annotatedAssetId": "asset ID",
+    "notes": "notes",
+    "model": "model",
+    "meid": "meid",
+    "orderNumber": "order number",
+    "willAutoRenew": true,
+    "osVersion": "os version",
+    "platformVersion": "plataform version",
+    "firmwareVersion": "firmware version",
+    "macAddress": "mac address",
+    "bootMode": "boot mode",
+    "lastEnrollmentTime": "last enrolled date",
+    "orgUnitPath": "org unit path"
+    }""";
+
+  /// Group of tests to do to the api calls, each test has its objective
+  group('Api Calls', () {
+    test(
+        'returns AccountDevices if the http call completes successfully without next page token',
+        () async {
+      final client = MockClient();
+      when(client.get(ApiCalls.PREFIX + ApiCalls.DEVICE_LIST_URL,
+              headers: {HttpHeaders.authorizationHeader: 'Bearer $authToken'}))
+          .thenAnswer((_) async => http.Response(jsonAccountDevices, 200));
+      final apiCall = ApiCalls(client);
+
+      expect(
+          await apiCall.getDeviceList(authToken, null), isA<AccountDevices>());
+    });
+
+    test(
+        "throws an exception if the http call for list of devices without next page token completes with an error",
+        () {
+      final client = MockClient();
+      when(client.get(ApiCalls.PREFIX + ApiCalls.DEVICE_LIST_URL,
+              headers: {HttpHeaders.authorizationHeader: 'Bearer $authToken'}))
+          .thenAnswer((_) async => http.Response('Not Found', 404));
+      final apiCall = ApiCalls(client);
+
+      expect(apiCall.getDeviceList(authToken, null), throwsException);
+    });
+
+    test(
+        'returns AccountDevices if the http call completes successfully with next page token',
+        () async {
+      final client = MockClient();
+      when(client.get(
+          ApiCalls.PREFIX + ApiCalls.DEVICE_LIST_WITH_TOKEN_URL + nextPageToken,
+          headers: {
+            HttpHeaders.authorizationHeader: 'Bearer $authToken'
+          })).thenAnswer((_) async => http.Response(jsonAccountDevices, 200));
+      final apiCall = ApiCalls(client);
+
+      expect(await apiCall.getDeviceList(authToken, nextPageToken),
+          isA<AccountDevices>());
+    });
+    test(
+        "throws an exception if the http call for list of devices with next page token completes with an error",
+        () {
+      final client = MockClient();
+      when(client.get(
+          ApiCalls.PREFIX + ApiCalls.DEVICE_LIST_WITH_TOKEN_URL + nextPageToken,
+          headers: {
+            HttpHeaders.authorizationHeader: 'Bearer $authToken'
+          })).thenAnswer((_) async => http.Response('Not Found', 404));
+      final apiCall = ApiCalls(client);
+
+      expect(apiCall.getDeviceList(authToken, nextPageToken), throwsException);
+    });
+
+    test(
+        'returns DetailedDevice if the http call to detailed device completes successfully',
+        () async {
+      final client = MockClient();
+      when(client.get(ApiCalls.PREFIX + ApiCalls.DETAILED_DEVICE_URL + deviceId,
+              headers: {HttpHeaders.authorizationHeader: 'Bearer $authToken'}))
+          .thenAnswer((_) async => http.Response(jsonDetailedDevice, 200));
+      final apiCall = ApiCalls(client);
+      expect(await apiCall.getDetailedDevice(authToken, deviceId),
+          isA<DetailedDevice>());
+    });
+    test(
+        "throws an exception if the http call for detailed device completes with an error",
+        () {
+      final client = MockClient();
+      when(client.get(ApiCalls.PREFIX + ApiCalls.DETAILED_DEVICE_URL + deviceId,
+              headers: {HttpHeaders.authorizationHeader: 'Bearer $authToken'}))
+          .thenAnswer((_) async => http.Response('Not Found', 404));
+      final apiCall = ApiCalls(client);
+      expect(apiCall.getDetailedDevice(authToken, deviceId), throwsException);
+    });
+  });
+}
